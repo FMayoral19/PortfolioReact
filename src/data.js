5de6d95e7bd20932d7953e73a89a062d7ffe9b48
FMayoral19 <fcoalanmayoral@hotmail.com>
2023-11-19 17:03:33 -0700

    projects update

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        id: 1,
+        name: "Bernulli Simulation",
+        tools: "Matlab",
+        date: "Dicember 2022",
+        description: "we conducted a simulation of the draining of a water tank, applying principles of physics.",
+        image: "berlin.jpg",
+    },
+    {
+        id: 2,
+        name: "Bernulli Simulation",
+        tools: "Matlab",
+        date: "Dicember 2022",
+        description: "we conducted a simulation of the draining of a water tank, applying principles of physics.",
+        image: "berlin.jpg",
+    }
+]
